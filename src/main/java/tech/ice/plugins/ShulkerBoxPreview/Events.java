4f22537f9084df9879685c446b1a77b7c68268ad
TangJin <55261514+tangjin0418@users.noreply.github.com>
2023-08-31 23:28:24 +0800

    1. Client-side language display (with reflection) 2. Version to 1.17+ (pom output version to 16). 3. ItemStack#getTransition to LocaleQuery 4. Fix UTF-8 problem, now you don't need to add to startup flag. 5. That's all I can remember.

--- src/main/java/tech/ice/plugins/ShulkerBoxPreview/Events.java
+++ src/main/java/tech/ice/plugins/ShulkerBoxPreview/Events.java
@@ -3,32 +3,25 @@ package tech.ice.plugins.ShulkerBoxPreview;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.bukkit.Material;
-import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLocaleChangeEvent;
-import org.bukkit.event.server.ServerLoadEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLocaleChangeEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedInputStream;
 import java.io.IOException;
-
-import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
 import java.net.URL;
-
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
 import static tech.ice.plugins.ShulkerBoxPreview.Config.*;
@@ -99,7 +92,8 @@ public class Events implements Listener {
     }
 
     @EventHandler
-    public void onPlayerLocalChange(PlayerLocaleChangeEvent event) {
+    public void onPlayerLocaleChange(PlayerLocaleChangeEvent event) {
+        if (client_language) return;
         String locale = event.getLocale().toLowerCase();
         File dir = new File(ShulkerBoxPreview.getDataFolder() + "/langs");
         File file = new File(ShulkerBoxPreview.getDataFolder() + "/langs/" + locale + ".json");
@@ -110,7 +104,7 @@ public class Events implements Listener {
             try {
                 URL url = new URL(lang_lib + "/" + locale + ".json");
                 Files.copy(url.openStream(), Paths.get(file.getPath()));
-                Main.langs.put(locale, new Gson().fromJson(new InputStreamReader(url.openStream()), JsonObject.class));
+                Main.langs.put(locale, new Gson().fromJson(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8), JsonObject.class));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -126,36 +120,6 @@ public class Events implements Listener {
         if (latest == null) return;
         if (!Objects.equals(latest, ShulkerBoxPreview.getDescription().getVersion())) {
             event.getPlayer().sendMessage(String.format(check_update_notify_message, latest));
-        }
-    }
-
-    @EventHandler
-    public void onServerLoad(ServerLoadEvent event) {
-        if (event.getType().equals(ServerLoadEvent.LoadType.STARTUP)) {
-            File dir = new File(ShulkerBoxPreview.getDataFolder() + "/langs/");
-            String[] names = dir.list();
-            if (names == null) return;
-            for (String name : names) {
-                try {
-                    File file = new File(ShulkerBoxPreview.getDataFolder() + "/langs/" + name);
-                    if (file.length() == 0 && file.delete()) break;
-                    URL url = new URL(lang_lib + "/" + name);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    if (connection.getResponseCode() != 200 && file.delete()) break;
-                    BufferedInputStream inputStream1 = new BufferedInputStream(new FileInputStream(file));
-                    BufferedInputStream inputStream2 = new BufferedInputStream(connection.getInputStream());
-                    int data;
-                    while ((data = inputStream1.read()) != -1) {
-                        if (data != inputStream2.read()) {
-                            Files.copy(url.openStream(), Paths.get(file.getPath()), StandardCopyOption.REPLACE_EXISTING);
-                            break;
-                        }
-                    }
-                    Main.langs.put(name.replace(".json", ""), new Gson().fromJson(new InputStreamReader(url.openStream()), JsonObject.class));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 }
